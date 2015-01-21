e83e46fd17288ae19402a32852434de30330175e
root <root@iZ257wm7n58Z.(none)>
2015-01-21 15:01:42 +0800

    nd

--- index.php
+++ index.php
@@ -5,13 +5,12 @@
 		'host' => '127.0.0.1',
 		'port' => 6379,
 	));
-
-	$link = mysql_connect('127.0.0.1','root','880829l');
+    echo "<meta charset='utf-8'>";
+	$link = mysql_connect('127.0.0.1','root','880829l') or die('there is no links on deamon!');
 	mysql_select_db('baike',$link);
 	$sql = "select * from jk_words";
 	$res = mysql_query($sql);
 	while($rows = mysql_fetch_assoc($res)){
-		
 		$ress = $redis->hset('doc:'.$rows['id'],'summary',$rows['summary'],'cid',$rows['cid'],'scontent',$rows['scontent'],'mulu',$rows['mulu']);
 		if($ress){
 			echo "yes <br/>";
