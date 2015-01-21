92e769ad081891717c1914f491d2abce0a5de63c
mangle <gotaotao123@163.com>
2015-01-21 13:28:27 +0800

    asdf
    dsfasdf

--- index.php
+++ index.php
@@ -11,8 +11,13 @@
 	$sql = "select id,summary from jk_words";
 	$res = mysql_query($sql);
 	while($rows = mysql_fetch_assoc($res)){
-		$ress = $redis->set('doc:'.$rows['id'],'summary',$rows['summary']);
-		echo "yes <br/>";
+		$ress = $redis->hset('doc:'.$rows['id'],'summary',$rows['summary']);
+		if($ress){
+			echo "yes <br/>";
+		}else{
+			echo "no <br/>";
+		}
+		
 	}
 
     
