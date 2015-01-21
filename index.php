278d4d01e3481539e151b326352e1fd321160b13
mangle <gotaotao123@163.com>
2015-01-21 13:23:58 +0800

    asdfasdf
    sadfasdf

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
 		'port' => 6379,
 	));
 
-	echo $redis->get('name');
+	$link = mysql_connect('127.0.0.1','root','880829l');
+	mysql_select_db('baike',$link);
+	$sql = "select id,summary from jk_words";
+	$res = mysql_query($sql);
+	while($rows = mysql_fetch_row($res)){
+		echo "<pre>";
+		var_dump($rows);
+		exit;
+		$ress = $redis->set('doc:'.$rows['id'],'summary',$rows['summary']);
+		echo "yes <br/>";
+	}
+
     
 
